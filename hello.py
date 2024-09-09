9602d304f25994bba4001607acf4cfe6c6003978
pradeept <pradeep.tarakar17@gmail.com>
2024-09-09 22:28:25 +0530

    add python script with dep

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-print("Bye world!")
-print("another")
+import pyfiglet
+
+result = pyfiglet.figlet_format("0X0B2")
+print(result)
+
+print("Let's learn the 'Philosophy' in a Indian way! :) \n")
+print("Astik (orthodox) - Vedanta, Mimansa, Samkhya, Yoga, Nyaya")
+print("Nastik (heterodox) - Bauddha, Jain, Charvaka ")
